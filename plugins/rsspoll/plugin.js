f6483eaa750410da27b4b7748ca5bdf4c16be7a7
Tony Garnock-Jones <tonyg@lshift.net>
2009-03-27 17:46:41 +0000

    Start thinking about plugin description

---
+++ plugins/rsspoll/plugin.js
@@ -0,0 +1,11 @@
+{
+    "name": "RSS Poller",
+    "author": {"name": "Michael Bridgen"},
+
+    "type": "plugin",
+    "harness": "python",
+    "subtype": "pipeline_component",
+    
+    "inputs": {},
+    "outputs": {"output": {}}
+}
