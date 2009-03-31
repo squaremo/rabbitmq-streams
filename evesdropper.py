7cda2101f6dd9f35e68db406a138f70c11c36f00
Michael Bridgen <mikeb@lshift.net>
2009-03-31 19:17:37 +0100

    A Python script that mimics the orchestrator wiring to evesdrop on inter-component messages

---
+++ evesdropper.py
@@ -0,0 +1,46 @@
+import sys, os.path
+sys.path.append("harness/python/lib")
+
+import amqplib.client_0_8 as amqp
+import glob
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+connection = amqp.Connection(host="localhost:5672", userid="feedshub_admin", password="feedshub_admin")
+channel = connection.channel()
+
+def subscribe(feed, outcomp, outname, incomp, inname):
+    def printMsg(msg):
+        print "Message in %s:" % feed
+        print "%s/%s -> %s/%s" % (outcomp, outname, incomp, inname)
+        print msg.body
+    exchange='%s_%s_%s' % (feed, outcomp, outname)
+    q, mc, cc = channel.queue_declare()
+    channel.queue_bind(q, exchange)
+    channel.basic_consume(q, callback=printMsg)
+
+print "Active component configs:"
+for configdoc in glob.glob("test_data/feedshub_config/*"):
+    config = json.loads(open(configdoc).read())
+    configname = os.path.splitext(os.path.basename(configdoc))[0]
+    if config['active']:
+        print "Feed %s is active" % configname
+        nodes = config['wiring']['nodes']
+        for node in nodes:
+            plugindoc = 'plugins/%s/plugin.js' % (nodes[node]['type'])
+            print "Loading plugin for %s (%s)" % (nodes[node]['type'], plugindoc)
+            plugin = json.loads(open(plugindoc).read())
+            print "Component %s_%s" % (configname, node)
+            for inp in plugin['inputs']:
+                print "  Input queue: %s_%s_%s" % (configname, node, inp['name'])
+            for outp in plugin['outputs']:
+                print "  Output exchange: %s_%s_%s" % (configname, node, outp['name'])
+        for edge in config['wiring']['edges']:
+            subscribe(configname, *edge)
+    else:
+        print "Feed %s not active" % configname
+
+while True:
+    channel.wait()
