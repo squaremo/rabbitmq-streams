098708e0898765fd47619ecbaa4091a0365b74b0
Michael Bridgen <mikeb@lshift.net>
2009-04-02 15:42:51 +0100

    Sample interleaving feed

---
+++ test_data/feedshub_config/bfc7f6869bbe5e25fb029c89699b05fb62c59ce8.js
@@ -0,0 +1,27 @@
+{
+    "type": "feed",
+    "active": true,
+    "user": "testuser",
+    "wiring": {
+	"nodes": {
+	    "the_input1": {
+	      "type": "rsspoll",
+              "configuration": {"href": "http://www.lshift.net/blog/feed",
+                                "interval": 120}
+	    },
+	    "the_input2": {
+	      "type": "rsspoll",
+              "configuration": {"href": "http://blog.feedshub.lshift.net/?feed=rss2",
+                                "interval": 120}
+	    },
+	    "the_output": {
+	      "type": "webfeed",
+              "configuration": {"title": "LShift blog again"}
+	    }
+	},
+	"edges": [
+	  ["the_input1", "output", "the_output", "input"],
+          ["the_input2", "output", "the_output", "input"]
+	]
+    }
+}
