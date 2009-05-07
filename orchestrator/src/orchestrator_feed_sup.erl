70fccbe49713fede37235cfc4239ead5e79717a3
Matthew Sackman <matthew@lshift.net>
2009-05-07 16:59:02 +0100

    and it runs!
    --HG--
    branch : bug20673

--- orchestrator/src/orchestrator_feed_sup.erl
+++ orchestrator/src/orchestrator_feed_sup.erl
@@ -9,7 +9,9 @@
 
 start_link(FeedId) when is_binary(FeedId) ->
     supervisor:start_link({local, list_to_atom("Feed_" ++ binary_to_list(FeedId))},
-			  ?MODULE, [FeedId]).
+			  ?MODULE, [FeedId]);
+start_link(FeedId) when is_list(FeedId) ->
+    start_link(list_to_binary(FeedId)).
 
 init([FeedId]) ->
     {ok, {{one_for_all, 10, 10},
