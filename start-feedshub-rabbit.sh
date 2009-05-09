988ade3df9784c5dd5e5d14c7baac637793f7ade
Tony Garnock-Jones <tonyg@lshift.net>
2009-05-09 14:54:00 +0100

    Use proper {} instead of () for sh var expansion

--- start-feedshub-rabbit.sh
+++ start-feedshub-rabbit.sh
@@ -3,7 +3,7 @@
 export RABBITMQ_LOG_BASE="`pwd`/build/logs/rabbitmq"
 export RABBITMQ_MNESIA_BASE="`pwd`/build/db/rabbitmq"
 export RABBITMQ_NODE_ONLY=true
-export RABBITMQ_SERVER_START_ARGS="$(RABBITMQ_SERVER_START_ARGS) -s rabbit"
+export RABBITMQ_SERVER_START_ARGS="${RABBITMQ_SERVER_START_ARGS} -s rabbit"
 
 mkdir -p "$RABBITMQ_LOG_BASE"
 
