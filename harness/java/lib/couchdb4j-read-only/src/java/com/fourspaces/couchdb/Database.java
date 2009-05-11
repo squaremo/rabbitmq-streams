16a4270ceeb395230b501f6777a08340281ea23b
Michael Bridgen <mikeb@lshift.net>
2009-05-11 02:53:31 +0100

    Change the view paths to match CouchDB 0.9

--- harness/java/lib/couchdb4j-read-only/src/java/com/fourspaces/couchdb/Database.java
+++ harness/java/lib/couchdb4j-read-only/src/java/com/fourspaces/couchdb/Database.java
@@ -44,7 +44,9 @@ public class Database {
 	
 	private Session session;
 	
-	private static final String VIEW = "_view";
+	private static final String VIEW = "/_view/";
+        private static final String DESIGN = "_design/";
+  
 	
 	/**
 	 * C-tor only used by the Session object.  You'd never call this directly.
@@ -131,7 +133,8 @@ public class Database {
   private ViewResults view(final View view, final boolean isPermanentView) {
     String url = null;
     if (isPermanentView) {
-      url = this.name + "/" + VIEW + "/" + view.getFullName();
+      String[] elements = view.getFullName().split("/");
+      url = this.name + "/" + ((elements.length < 2) ? elements[0] : DESIGN + elements[0] + VIEW + elements[1]);
     } else {
       url = this.name + "/" + view.getFullName();
     }
