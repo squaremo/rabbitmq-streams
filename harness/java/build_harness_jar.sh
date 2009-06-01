5d24fdeaf9e1b909c75f881c2a7f4571d2323604
Alexander Schmolck <alexander@lshift.net>
2009-06-01 19:07:46 +0100

    Robustified build_harness_jar.sh (max cmdline args exceeded on EC2 jaunty etc.)

--- harness/java/build_harness_jar.sh
+++ harness/java/build_harness_jar.sh
@@ -1,33 +1,35 @@
 #! /bin/sh
-
-DIR=$(pwd)
-PATH_TO_ME=$(dirname $0)
-cd $PATH_TO_ME
-
+set -e
+IFS='
+'
+DIR="$(pwd)"
+PATH_TO_ME="$(dirname -- "$0")"
+cd -- "$PATH_TO_ME"
 SCRATCH="../../build/scratch/"$$
-mkdir $SCRATCH
+mkdir -- "$SCRATCH"
 
-cp -R bin/* $SCRATCH
+cp -R -- bin/* "$SCRATCH"
 
 for j in $(find lib -type f -name "*.jar")
 do
-    echo $j
-    cp -R $j $SCRATCH
-    cd $SCRATCH
-    jar xf $(basename $j)
-    rm $(basename $j)
-    cd $DIR
+    echo "$j"
+    cp -R -- "$j" "$SCRATCH"
+    cd -- "$SCRATCH"
+    jar xf "$(basename $j)"
+    rm -- $(basename $j)
+    cd -- "$DIR"
 done
 
-cd $SCRATCH
+cd -- "$SCRATCH"
 rm -rf META-INF
-jar cf feedshub_harness.jar $(find . -name '*.class' -type f)
-cd $DIR
+touch .make-jar-happy; jar cf feedshub_harness.jar .make-jar-happy
+find . -name '*.class' -type f -print0 | xargs -0 jar uf feedshub_harness.jar
+cd -- "$DIR"
 
-cp $SCRATCH/feedshub_harness.jar .
+cp -- "$SCRATCH/feedshub_harness.jar" .
 zip -Tv feedshub_harness.jar
 
-rm -rf $SCRATCH
-cd $DIR
+rm -rf -- "$SCRATCH"
+cd -- "$DIR"
 
-echo Jar created in $PATH_TO_ME/feedshub_harness.jar
+echo "Jar created in $PATH_TO_ME/feedshub_harness.jar"
