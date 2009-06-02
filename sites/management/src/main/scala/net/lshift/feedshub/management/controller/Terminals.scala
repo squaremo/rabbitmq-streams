164a135cda26ac2fc7f3244b8523b06e42cb9fb4
Tim Clark <tim@lshift.net>
2009-06-02 16:55:37 +0100

    Potential fix to the broken sources and destinations page - if I am understanding things correctly.

--- sites/management/src/main/scala/net/lshift/feedshub/management/controller/Terminals.scala
+++ sites/management/src/main/scala/net/lshift/feedshub/management/controller/Terminals.scala
@@ -12,56 +12,54 @@ import scala.actors.Actor._
 import com.fourspaces.couchdb._
 import net.sf.json.JSONObject
 
+case class TerminalStatus(id: String, active: Boolean, source: Boolean, destination: Boolean, server: String)
 
-case class TerminalStatus(id : String, active: Boolean, source : Boolean, destination: Boolean, server: String)
-
-case class UpdateTerminalsList(terminals : List[TerminalStatus])
+case class UpdateTerminalsList(terminals: List[TerminalStatus])
 
 object Terminals extends Actor with FeedsHubConfig with ConfigAwareActor with ObservableActor[UpdateTerminalsList] {
-    val TerminalStatusView = "terminals/join?group=true"
-    
-    override def bindingKey : String = "*.*" // i.e., only things with two components
+  val TerminalStatusView = "terminals/join?group=true"
 
-    var terminalstata : List[TerminalStatus] = Nil
+  override def bindingKey: String = "*.*" // i.e., only things with two components
 
-    private def terminalStatusFromJSON(id: String, json : JSONObject) : TerminalStatus = {
-        val status = json.getJSONObject("terminal-status")
-        val config = json.getJSONObject("terminal")
-        val source = config.has("source")
-        val destination = config.has("destination")
-        TerminalStatus(id,
-                       status.getBoolean("active"),
-                       source,
-                       destination,
-                       config.getString("server"))
+  var terminalstata: List[TerminalStatus] = Nil
+
+  private def terminalStatusFromJSON(id: String, json: JSONObject): TerminalStatus = {
+    val status = json.getJSONObject("terminal-status")
+    val config = json.getJSONObject("terminal")
+    val source = config.has("source")
+    val destination = config.has("destination")
+    val servers = config.getJSONArray("servers")
+    val firstServer = servers.getJSONObject(0)
+    val server = firstServer.get("server").toString
+    TerminalStatus(id, status.getBoolean("active"), source, destination, server)
+  }
+
+  def readStatus {
+    val vr = statusDb.view(TerminalStatusView).getResults
+    terminalstata = vr.map(v => terminalStatusFromJSON(v.getString("key"), v.getJSONObject("value"))).toList
+  }
+
+  def newObserver(newbie: Actor) {
+    newbie ! UpdateTerminalsList(terminalstata)
+  }
+
+  def notifyOfUpdate {
+    notifyObservers(UpdateTerminalsList(terminalstata))
+  }
+
+  val handleChanges: PartialFunction[Any, Unit] = {
+    case ConfigChange(id) =>
+      true // ignore for now
+    case StatusChange(id) =>
+      readStatus; notifyOfUpdate
+  }
+
+  def act {
+    loop {
+      react(handleChanges orElse handleObservers)
     }
+  }
 
-    def readStatus {
-        val vr = statusDb.view(TerminalStatusView).getResults
-        terminalstata = vr.map(v => terminalStatusFromJSON(v.getString("key"), v.getJSONObject("value"))).toList
-    }
-
-    def newObserver(newbie : Actor) {
-        newbie ! UpdateTerminalsList(terminalstata)
-    }
-
-    def notifyOfUpdate {
-        notifyObservers(UpdateTerminalsList(terminalstata))
-    }
-
-    val handleChanges : PartialFunction[Any, Unit] = {
-        case ConfigChange(id) =>
-            true // ignore for now
-        case StatusChange(id) =>
-            readStatus; notifyOfUpdate
-    }
-
-    def act {
-        loop {
-            react (handleChanges orElse handleObservers)
-        }
-    }
-
-    readStatus
-    start
+  readStatus
+  start
 }
