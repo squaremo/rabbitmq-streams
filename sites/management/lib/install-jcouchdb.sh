9a0361f7031ac66efe31ebd5266423b17c68f46a
Tim Clark <tim@lshift.net>
2009-06-03 12:38:13 +0100

    Removed outdated libs from management application

--- sites/management/lib/install-jcouchdb.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -DgroupId=jcouchdb -DartifactId=jcouchdb -Dversion=0.9.0-2 -Dpackaging=jar -Dfile=jcouchdb-0.9.0-2.jar
-mvn install:install-file -DgroupId=net.svenson -DartifactId=svenson -Dversion=1.3.0 -Dpackaging=jar -Dfile=svenson-1.3.0.jar
--- sites/management/lib/install-rabbitmq-client.sh
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -DgroupId=net.rabbitmq -DartifactId=amqp-client -Dversion=1.5.4 -Dpackaging=jar -Dfile=rabbitmq-client-1.5.4.jar
