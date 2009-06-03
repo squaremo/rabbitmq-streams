8e0f7670bb3067be0f7c95e8a1c7dbded7eae94e
Tim Clark <tim@lshift.net>
2009-06-03 15:10:38 +0100

    Basic display of archive content functional.

--- sites/management/src/main/scala/net/lshift/feedshub/management/comet/ArchivesActor.scala
+++ sites/management/src/main/scala/net/lshift/feedshub/management/comet/ArchivesActor.scala
@@ -4,23 +4,32 @@ package net.lshift.feedshub.management.comet
 import liftweb.http._
 import liftweb.util.Full
 import liftweb.widgets.tablesorter.TableSorter
-
+import model.{Archive, LocalServer}
+import scala.xml._
 class ArchivesActor extends CometActor {
 
-  var records: List[String] = Nil
+  var archives: List[Archive] = Nil
 
   override def defaultPrefix = Full("a")
 
   override def render: RenderOut = {
     bind("list" ->(
-      <table id="archive-list" class="tablesorter">
-        <thead>
-          <tr><th>Date</th><th>Archive</th></tr>
-        </thead>
-        <tbody>
-          {records.map(r => <tr><td>Some date</td><td>{r}</td></tr>)}
-        </tbody>
-      </table>
-      <script>{TableSorter.jsRender("feeds-list")}</script>))
+      <div>
+      {archives.map(a =>
+        <h3>{a.name} - {a.title}</h3>
+        <table>
+          <thead>
+            <tr><th>Updated</th><th>Content</th></tr>
+          </thead>
+          <tbody>
+            {a.entries(10).map(e => <tr><td>{e.updated}</td><td>{e.content}</td></tr>)}
+          </tbody>
+        </table>)
+      }
+    </div>))
+  }
+
+  override protected def localSetup() = {
+    archives = LocalServer.archives.toList
   }
 }
