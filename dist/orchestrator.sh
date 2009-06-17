df4859bbf5a0d2891a05044cbaa012b3da9f7ac3
Tim Clark <tim@lshift.net>
2009-06-17 11:46:42 +0100

    Removing directory that is no longer necessary for rpm creation

--- dist/orchestrator.sh
+++
@@ -1,7 +0,0 @@
-erl -pa LIB_DIR/orchestrator/ebin \
-    -pa LIB_DIR/erlang-rfc4627/ebin \
-    -pa LIB_DIR/rabbitmq-erlang-client/ebin \
-    -pa LIB_DIR/rabbitmq/ebin \
-    -pa LIB_DIR/ibrowse/ebin \
-    -boot start_sasl \
-    -sname orchestrator -s orchestrator
