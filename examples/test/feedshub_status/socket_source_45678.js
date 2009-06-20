8c3f3dce90370a3bd9df27a17eead56289a9e977
Michael Bridgen <mikeb@lshift.net>
2009-06-20 23:52:36 +0100

    Remove vestigal terminals in example
    --HG--
    branch : experimental_dynamic_config

--- examples/test/feedshub_status/socket_source_45678.js
+++
@@ -1,8 +0,0 @@
-{
-    "type": "terminal",
-    "servers": [ {"server": "socket_source_server",
-                  "source": {"port": 45678}},
-                 {"server": "socket_source_server",
-                  "source": {"port": 45679}}
-               ]
-}
