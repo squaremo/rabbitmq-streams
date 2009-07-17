a8f4c32319364924186e72582d13f27ee1abe16f
Lee Coomber <lee@lshift.net>
2009-07-17 11:37:22 +0100

    Add crude message rate limiting
    --HG--
    branch : perf_testing

--- test/load/send_http_post.py
+++ test/load/send_http_post.py
@@ -10,22 +10,36 @@ URL = grinder.properties.getProperty("streams.test.url")
 
 MSG_SIZE = grinder.properties.getInt("streams.test.msg_size", 128)
 
+# The target message rate per second. The implementation of this is crude and
+# this figure will actually be a maximum
+MSG_RATE = grinder.properties.getDouble("streams.test.msg_rate", 0)
+
 log = grinder.logger.output
+
+sleepTime = 0
+if MSG_RATE > 0:
+    sleepTimeSec = 1 / MSG_RATE
+    sleepTime = sleepTimeSec * 1000
 
 class TestRunner:
     # TODO: Some comments and explanation
 
     def __call__(self):
+        t1 = System.currentTimeMillis()
+
         test = Test(1, "HTTP post")
         request = test.wrap(HTTPRequest())
 
         timestamp = "|timestamp=" + str(System.currentTimeMillis()) + "|"
         padding = 'X' * (MSG_SIZE - len(timestamp))
         data = timestamp + padding + '\n'
-        
+
         result = request.POST(URL, data)
 
         if not result.statusCode == 204:
             raise Exception("Unexpected HTTP response; " + result.getText())
 
-        
+        if sleepTime > 0:
+            # Adjust sleep time based on test time
+            adjustedSleeptime = sleepTime - (System.currentTimeMillis() - t1)
+            grinder.sleep(long(adjustedSleeptime))
