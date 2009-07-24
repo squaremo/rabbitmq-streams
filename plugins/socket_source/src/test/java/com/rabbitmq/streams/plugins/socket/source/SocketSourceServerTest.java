e0ffe27f8d50b52c458529a61807a259164438bb
Tim Clark <tim@lshift.net>
2009-07-24 09:54:43 +0100

    Unit test for socket source
    --HG--
    branch : bug21191

---
+++ plugins/socket_source/src/test/java/com/rabbitmq/streams/plugins/socket/source/SocketSourceServerTest.java
@@ -0,0 +1,104 @@
+package com.rabbitmq.streams.plugins.socket.source;
+
+import com.rabbitmq.streams.harness.MessageChannel;
+import com.rabbitmq.streams.harness.PluginBuildException;
+import com.rabbitmq.streams.harness.Message;
+import com.rabbitmq.streams.harness.MessagingException;
+import net.sf.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import org.mockito.ArgumentMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.net.UnknownHostException;
+import java.net.Socket;
+import java.net.InetAddress;
+
+public class SocketSourceServerTest {
+
+  private SocketSourceServer server;
+  private MessageChannel channel;
+  private static final String SAUSAGES = "SAUSAGES";
+
+  @Before
+  public void setup() throws PluginBuildException {
+    channel = mock(MessageChannel.class);
+
+    server = new SocketSourceServer();
+    server.setMessageChannel(channel);
+
+    server.configure(null);
+
+  }
+
+  @Test
+  public void testReceive() throws MessagingException {
+    JSONObject terminal = new JSONObject();
+    terminal.put("source", JSONObject.fromObject("{\"port\":20468}"));
+    List<JSONObject> terminals = new ArrayList<JSONObject>();
+    terminals.add(terminal);
+
+    server.terminalStatusChange("socket_source_server", terminals, true);
+    writeToSocket(SAUSAGES);
+    Message message = mock(Message.class);
+    ArgumentMatcher<Message> matcher = new IsMessageThatMatchesContent(SAUSAGES);
+    verify(channel).publish(eq("output"), argThat(matcher));
+  }
+
+  private class IsMessageThatMatchesContent extends ArgumentMatcher  {
+    private String match;
+
+    public IsMessageThatMatchesContent(String match) {
+      this.match = match;
+    }
+
+    public boolean matches(Object o) {
+      if(o instanceof Message)  {
+        Message message = (Message) o;
+        return match.equals(new String(message.body()));
+      }
+      return false;
+    }
+  }
+  
+  private void writeToSocket(String content) {
+    BufferedWriter wr = null;
+    Socket socket = null;
+    try {
+      InetAddress addr = InetAddress.getByName("localhost");
+      int port = 20468;
+
+      socket = new Socket(addr, port);
+
+      wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+      wr.write(content);
+      wr.flush();
+
+    }
+    catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        if (wr != null) {
+          wr.close();
+        }
+        if (socket != null) {
+          socket.close();
+        }
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
+}
