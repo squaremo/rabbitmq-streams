398ae0d5987855dae8a8e1df1082cc3d186345ed
Michael Bridgen <mikeb@lshift.net>
2009-07-26 19:35:34 +0100

    Change regexp_replace plugin to expect a list of regexp->replacement, and apply them sequentially
    --HG--
    branch : bug21253

--- plugins/regexp_replace/regexp_replace.py
+++ plugins/regexp_replace/regexp_replace.py
@@ -2,32 +2,41 @@ from feedshub import Component
 from feedshub import json
 import re
 
+LOCALFLAGS = ('multiline', 'caseinsensitive', 'dotall')
+GLOBALFLAGS = ('global_multiline', 'global_caseinsensitive', 'global_dotall')
+
+def makeFlags(config, defaults=(False, False, False), keys=LOCALFLAGS):
+    keyAndDefault = zip(keys, defaults)
+    multiline, caseinsensitive, dotall = \
+        tuple(config.get(*kd) for kd in keyAndDefault)
+    f = 0
+    if multiline:
+        f |= re.MULTILINE
+    if caseinsensitive:
+        f |= re.IGNORECASE
+    if dotall:
+        f |= re.DOTALL
+    return f
+
 class RegexpReplacer(Component):
 
     def __init__(self, config):
         super(RegexpReplacer, self).__init__(config)
+
+    def matcher(self, expression, globalFlags):
+        match = expression["regexp"]
+        flags = makeFlags(expression, defaults=globalFlags)
+        return re.compile(match, flags)
         
     def input(self, msg, config):
-        match = config["regexp"]
-        replacement = config["replacement"]
-        multiline = config["multiline"]
-        caseinsensitive = config["caseinsensitive"]
-        dotall = config["dotall"]
-
-        flags = 0
-        if multiline:
-            flags |= re.MULTILINE
-        if caseinsensitive:
-            flags |= re.IGNORECASE
-        if dotall:
-            flags |= re.DOTALL
-        regexp = re.compile(match, flags)
+        globalFlags = [config.get(k, False) for k in GLOBALFLAGS]
         body = msg.body
-        result, count = re.subn(regexp, replacement, body)
-        if 0 == count:
-            self.negative(body)
-        else:
-            self.positive(result)
+        for expression in config["expressions"]:
+            match = self.matcher(expression, globalFlags)
+            replacement = expression["replacement"]
+            result, _ = re.subn(match, replacement, body)
+            body = result
+        self.output(body)
 
 def run(config):
     rr = RegexpReplacer(config)
