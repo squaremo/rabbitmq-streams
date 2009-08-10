ec760e5555ceb1dea5cdaede7c519a643d35f756
Tim Clark <tim@lshift.net>
2009-08-10 09:18:53 +0100

    Further js stashed for archiver

--- plugins/archive/dateview.js
+++ plugins/archive/dateview.js
@@ -10,3 +10,7 @@ function(doc) {
 }
 
 
+/*
+ * Apparently this should subset the view: startkey=[1,2009,5]&endkey=[1,2009,5,{}]#
+ * For example http://localhost:5984/archive_outtestarchive/_design/by_date/_view/By%20Date?startkey=[2007]&endkey=[2009,{}]
+ */
