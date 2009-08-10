33d7eb1f1394fe64cbf0519ecb67d46fc1f566e4
Michael Bridgen <mikeb@lshift.net>
2009-08-10 12:54:32 +0100

    Update plugin descriptor to match new parameters

--- plugins/notification/plugin.js
+++ plugins/notification/plugin.js
@@ -7,7 +7,13 @@
     "subtype": "server",
     
     "global_configuration_specification": [],
-    "configuration_specification": [],
+    "configuration_specification": [
+      {"label": "Host", "name": "host", "type": "hostname"},
+      {"label": "Port", "name": "port", "type": "integer < 65535"},
+      {"label": "Virtual host", "name": "virtual_host", "type": "string"},
+      {"label": "Username", "name": "username", "type": "string"},
+      {"label": "Password", "name": "password", "type": "string"}
+    ],
 
     "source_specification": [{"label": "Key", "type": "string", "name": "bindingkey"}]
 }
