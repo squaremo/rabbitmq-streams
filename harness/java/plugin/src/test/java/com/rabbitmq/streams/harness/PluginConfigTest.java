8a264d3f4caf59456eb1891bed79e71060abb786
Michael Bridgen <mikeb@lshift.net>
2009-08-14 14:38:13 +0100

    Unit tests for merging configuration; it turns out that this was returning null, leading to NPEs in plugins that paid attention to dynamic configuration.

--- harness/java/plugin/src/test/java/com/rabbitmq/streams/harness/PluginConfigTest.java
+++ harness/java/plugin/src/test/java/com/rabbitmq/streams/harness/PluginConfigTest.java
@@ -1,17 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.rabbitmq.streams.harness;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.QueueingConsumer;
 import java.util.HashMap;
 import java.util.Map;
 import net.sf.json.JSONArray;
+import net.sf.json.JSONNull;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import net.sf.json.JSONObject;
+
+import org.junit.Before;
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -19,14 +19,30 @@ import net.sf.json.JSONObject;
  */
 public class PluginConfigTest {
 
+  JSONObject values;
+  JSONObject conf;
+  QueueingConsumer queue;
+  Logger log;
+  InputHandler handler;
+  Map<String, Object> headers;
+
+  @Before
+  public void setUp() {
+    values = new JSONObject();
+    conf = new JSONObject();
+    queue = mock(QueueingConsumer.class);
+    log = mock(Logger.class);
+    handler = mock(InputHandler.class);
+    headers = new HashMap();
+  }
+
   /**
    * Test of propertiesWithHeaders method, of class Plugin.
    */
   @Test
   public void testPropertiesWithHeaders() {
-    Map<String, Object> hdrs = new HashMap();
-    hdrs.put("foo", "bar");
-    BasicProperties props = AMQPPublisher.propertiesWithHeaders(hdrs);
+    headers.put("foo", "bar");
+    BasicProperties props = AMQPPublisher.propertiesWithHeaders(headers);
     assertNotNull(props);
     assertNotNull(props.headers);
     assertEquals(props.deliveryMode, new Integer(2));
@@ -35,10 +51,8 @@ public class PluginConfigTest {
 
   @Test
   public void testSetValues() {
-    Map<String, Object> headers = new HashMap();
-    JSONObject vals = new JSONObject();
-    AMQPInputConsumer.setValuesInHeader(headers, vals);
-    assertEquals(vals, AMQPInputConsumer.getValuesFromHeader(headers));
+    AMQPInputConsumer.setValuesInHeader(headers, values);
+    assertEquals(values, AMQPInputConsumer.getValuesFromHeader(headers));
   }
 
   /**
@@ -46,12 +60,10 @@ public class PluginConfigTest {
    */
   @Test
   public void testSimpleInterpolateConfig() {
-    Map<String, Object> vals = new HashMap();
-    vals.put("foo", "bar");
-    JSONObject conf = new JSONObject();
+    values.put("foo", "bar");
     conf.put("interpolated", "$foo");
     conf.put("notinterpolated", "foo");
-    JSONObject interpolateConfig = AMQPInputConsumer.interpolateConfig(conf, vals);
+    JSONObject interpolateConfig = AMQPInputConsumer.interpolateConfig(conf, values);
     assertNotNull(interpolateConfig);
     assertEquals(interpolateConfig.size(), 2);
     assertEquals("bar", interpolateConfig.get("interpolated"));
@@ -60,13 +72,11 @@ public class PluginConfigTest {
 
   @Test
   public void testNestedMapInterpolateConfig() {
-    Map<String, Object> vals = new HashMap();
-    vals.put("foo", "bar");
-    JSONObject conf = new JSONObject();
+    values.put("foo", "bar");
     JSONObject subconf = new JSONObject();
     subconf.put("here", "$foo");
     conf.put("underhere", subconf);
-    JSONObject result = AMQPInputConsumer.interpolateConfig(conf, vals);
+    JSONObject result = AMQPInputConsumer.interpolateConfig(conf, values);
     assertNotNull(result);
     Object subresult = result.get("underhere");
     assertTrue(subresult instanceof JSONObject);
@@ -74,22 +84,78 @@ public class PluginConfigTest {
     assertEquals("bar", subresultjson.get("here"));
   }
 
-//  @Test
+  @Test
   public void testArrayInterpolateConfig() {
-    Map<String, Object> vals = new HashMap();
-    vals.put("foo", "bar");
-    JSONObject conf = new JSONObject();
+    values.put("foo", "bar");
     JSONArray subconf = new JSONArray();
     subconf.add("notthis");
     subconf.add("$foo");
     conf.put("underhere", subconf);
-    JSONObject result = AMQPInputConsumer.interpolateConfig(conf, vals);
+    JSONObject result = AMQPInputConsumer.interpolateConfig(conf, values);
     assertNotNull(result);
     Object subresult = result.get("underhere");
     assertTrue(subresult instanceof JSONArray);
     JSONArray subresultjson = (JSONArray)subresult;
     assertEquals("notthis", subresultjson.get(0));
     assertEquals("bar", subresultjson.get(1));
+  }
+
+  @Test
+  public void testNoopMerge() {
+    AMQPInputConsumer ic = new AMQPInputConsumer(queue, handler, conf, log) {
+
+      public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+    };
+    JSONObject merged = ic.mergeConfigWithHeaders(headers);
+    assertNotNull(merged);
+    assertTrue( ! JSONNull.getInstance().equals(merged));
+  }
+
+  @Test
+  public void testTrivialMerge() {
+    conf.put("foo", "bar");
+    AMQPInputConsumer ic = new AMQPInputConsumer(queue, handler, conf, log) {
+      public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+    };
+    JSONObject merged = ic.mergeConfigWithHeaders(headers);
+    assertNotNull(merged);
+    assertTrue( ! JSONNull.getInstance().equals(merged));
+    assertEquals(conf, merged);
+  }
+
+  @Test
+  public void testMergeWithMissingValue() {
+    conf.put("foo", "$bar");
+    AMQPInputConsumer ic = new AMQPInputConsumer(queue, handler, conf, log) {
+      public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+    };
+    JSONObject merged = ic.mergeConfigWithHeaders(headers);
+    assertNotNull(merged);
+    assertTrue( ! JSONNull.getInstance().equals(merged));
+    assertEquals("", merged.get("foo"));
+  }
+
+  @Test
+  public void testMergeWithValue() {
+    conf.put("foo", "$bar");
+    values.put("bar", "BAZ");
+    AMQPInputConsumer.setValuesInHeader(headers, values);
+    AMQPInputConsumer ic = new AMQPInputConsumer(queue, handler, conf, log) {
+      public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+      }
+    };
+    JSONObject merged = ic.mergeConfigWithHeaders(headers);
+    assertNotNull(merged);
+    assertTrue( ! JSONNull.getInstance().equals(merged));
+    assertEquals("BAZ", merged.get("foo"));
   }
 
 }
