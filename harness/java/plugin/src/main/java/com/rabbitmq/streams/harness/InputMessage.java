1829f99f8e514c2abdae47436e3398387f7c3aee
Alexander Schmolck <alexander@lshift.net>
2009-08-17 14:03:48 +0100

    bug21416: added .bodyAsString and .bodyAsStringLax to InputMessage.

--- harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/InputMessage.java
+++ harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/InputMessage.java
@@ -3,24 +3,41 @@
 
 package com.rabbitmq.streams.harness;
 
+import java.nio.charset.Charset;
+import java.nio.charset.CharacterCodingException;
+import java.nio.ByteBuffer;
 import java.util.Map;
 
 /**
  *
  * @author mikeb@lshift.net
+ * @author alexander@lshift.net
  */
 public abstract class InputMessage implements Message {
   public abstract InputMessage withHeader(String key, Object val);
   public abstract InputMessage withBody(byte[] body);
   public abstract InputMessage withHeaders(Map<String, Object> headers);
   public abstract void ack() throws MessagingException;
-
-  public InputMessage withBody(String body) {
-    return this.withBody(body.getBytes());
+  /** Silently replaces bad code points by '�' ('\ufffd')
+   */
+  public String bodyAsStringLax(){
+    //NB String(this.body(), "utf-8") is **not** the same.
+    return new String(this.body(), Charset.forName("utf-8"));
   }
-
+  /** This will complain about bad code points
+   */
+  public String bodyAsString() throws CharacterCodingException {
+    return Charset.forName("utf-8").decode(ByteBuffer.wrap(this.body())).toString();
+  }
+  public InputMessage withBody(String body) {
+    try {
+      return this.withBody(body.getBytes("utf-8"));
+    }
+    catch (java.io.UnsupportedEncodingException _){
+        throw new RuntimeException("This shouldn't happen.");
+    }
+  }
   public InputMessage withContentType(String contentType) {
     return this.withHeader("Content-Type", contentType);
   }
-
 }
