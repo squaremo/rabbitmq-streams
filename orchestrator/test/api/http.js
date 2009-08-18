8ca6c4ff922cbc048bf637abad8bd40f45169f77
Michael Bridgen <mikeb@lshift.net>
2009-08-18 18:31:16 +0100

    Add a test for the pipeline index
    --HG--
    branch : bug21412

--- orchestrator/test/api/http.js
+++ orchestrator/test/api/http.js
@@ -1,6 +1,6 @@
 var http = Packages.com.meterware.httpunit;
 
-function jsonGetResult(path) {
+function jsonGetResponse(path) {
   var c = new http.WebConversation();
   var req = new http.GetMethodWebRequest("http://localhost:8000"+path);
   var res = c.getResponse(req);
--- orchestrator/test/api/testModel.js
+++ orchestrator/test/api/testModel.js
@@ -7,7 +7,7 @@ Test.requires("http.js");
 Test.requires("json2.js");
 
 function test_root_get() {
-  var res = jsonGetResult('/');
+  var res = jsonGetResponse('/');
   var body = res.getText();
   var bodyObj = JSON.parse(new String(body));
   Test.areEqual("RabbitMQ Streams", bodyObj['application']);
@@ -15,4 +15,7 @@ function test_root_get() {
 }
 
 function test_pipeline_index() {
+  var res = jsonGetResponse('/model/pipeline/');
+  var json = JSON.parse(new String(res.getText()));
+  Test.isTrue(json.hasOwnProperty('/model/pipeline/modeltest'));
 }
