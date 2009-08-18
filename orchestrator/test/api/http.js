2537bded4a35bb43ec16975bc9a19264d7a7c994
Michael Bridgen <mikeb@lshift.net>
2009-08-18 15:56:27 +0100

    Compile the model defined in setup() -- but doesn't import it yet
    --HG--
    branch : bug21412

--- orchestrator/test/api/http.js
+++ orchestrator/test/api/http.js
@@ -1,1 +1,10 @@
 var http = Packages.com.meterware.httpunit;
+
+function jsonGetResult(path) {
+  var c = new http.WebConversation();
+  var req = new http.GetMethodWebRequest("http://localhost:8000"+path);
+  var res = c.getResponse(req);
+  Test.areEqual("application/json", res.getContentType());
+  Test.areEqual(200, res.getResponseCode());
+  return res;
+}
--- orchestrator/test/api/runner.sh
+++ orchestrator/test/api/runner.sh
@@ -2,4 +2,9 @@ CLASSPATH=
 for i in lib/*.jar; do CLASSPATH=$i:$CLASSPATH; done
 echo $CLASSPATH
 
-java -cp "$CLASSPATH" org.thinkpond.jstest.Main test*.js
+for t in test*.js; do
+    TARGET=`mktemp -d`
+    echo Compiling fixture to $TARGET
+    ../../../bin/compile.sh $TARGET stub.js $t setup.js
+    java -cp "$CLASSPATH" org.thinkpond.jstest.Main $t
+done
