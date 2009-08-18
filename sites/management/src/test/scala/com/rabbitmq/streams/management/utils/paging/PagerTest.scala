658383f5ef52617a1d2ab7e72a1371831ad43e3c
Tim Clark <tim@lshift.net>
2009-08-18 11:46:09 +0100

    Changed test from FuncSuite to JUnitSuite since maven surefire doesn't handle FuncSuite nicely

--- sites/management/src/test/scala/com/rabbitmq/streams/management/utils/paging/PagerTest.scala
+++ sites/management/src/test/scala/com/rabbitmq/streams/management/utils/paging/PagerTest.scala
@@ -1,48 +1,48 @@
 package com.rabbitmq.streams.management.utils.paging
 
-import org.scalatest.{FunSuite, Suite}
+import org.scalatest.junit.JUnit3Suite
 
-class PagerTest extends FunSuite {
-  test("0 is not a valid page size") {
+class PagerTest extends JUnit3Suite {
+  def testZeroIsNotAValidPageSize() {
     intercept[IllegalArgumentException] {
       new Pager(0, 0)
     }
   }
 
-  test("-1 is not a valid page size") {
+  def testMinusOneIsNotAValidPageSize() {
     intercept[IllegalArgumentException] {
       new Pager(-1, 0)
     }
   }
 
-  test("-1 is not a valid total size")  {
+  def testMinusOneIsNotAValidTotalSize()  {
     intercept[IllegalArgumentException]  {
       new Pager(1, -1)
     }
   }
 
-  test("A pager with a total size of 0 will have 0 pages")  {
+  def testAPagerWithATotalSizeOfZeroWillHaveZeroPages()  {
     expect(0) {
       val p = new Pager(10, 0)
       p.numberOfPages
     }
   }
 
-  test("A pager with total size of 10 and a page size of 10 will have 1 page")  {
+  def testAPagerWithTotalSizeOfTenAndAPageSizeOfTenWillHaveOnePage()  {
     expect(1) {
       val p = new Pager(10, 10)
       p.numberOfPages
     }
   }
 
-  test("A pager with total size of 8 and a page size of 10 will have 1 page")  {
+  def testAPagerWithTotalSizeOfEightAndAPageSizeOfTenWillHaveOnePage()  {
     expect(1) {
       val p = new Pager(10, 8)
       p.numberOfPages
     }
   }
 
-  test("A pager with total size of 24 and a page size of 10 will have 3 pages") {
+  def testAPagerWithTotalSizeOfTwentyFourAndAPageSizeOfTenWillHaveThreePages() {
     expect(3) {
       val p = new Pager(10, 24)
       p.numberOfPages
