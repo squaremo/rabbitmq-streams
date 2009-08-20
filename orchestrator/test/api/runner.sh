64418cdbc710df881b167adc1c2761cf66f52c55
Michael Bridgen <mikeb@lshift.net>
2009-08-20 17:09:55 +0100

    Allow the import script to treat its argument as a database, rather than a directory of databases, and use this in the test runner
    --HG--
    branch : bug21412

--- orchestrator/test/api/runner.sh
+++ orchestrator/test/api/runner.sh
@@ -3,8 +3,13 @@ for i in lib/*.jar; do CLASSPATH=$i:$CLASSPATH; done
 echo $CLASSPATH
 
 for t in test*.js; do
-    TARGET=`mktemp -d`
+    TMP=`mktemp -d`
+    DB=`basename $TMP | sed -e 's/\.//' |tr "[:upper:]" "[:lower:]"`
+    TARGET=`dirname $TMP`/$DB
+    mv $TMP $TARGET
     echo Compiling fixture to $TARGET
     ../../../bin/compile.sh $TARGET stub.js $t setup.js
+    ../../../sbin/import_config.py -f $TARGET
+    
     java -cp "$CLASSPATH" org.thinkpond.jstest.Main $t
 done
