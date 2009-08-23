10cffa03e959226e5b24f53de2affb124163db7a
Michael Bridgen <mikeb@lshift.net>
2009-08-23 15:53:25 +0100

    Test the process info API
    --HG--
    branch : bug21412

---
+++ orchestrator/test/api/testProcess.js
@@ -0,0 +1,27 @@
+var feedname = "testfeed";
+
+function setup() {
+  var f = feed(feedname);
+}
+
+Test.requires("json2.js");
+Test.requires("http.js");
+
+function test_pipeline_state() {
+  var url = '/process/pipeline/' + feedname;
+  var res = jsonGetResponse(url);
+  var body = res.getText();
+  var bodyObj = JSON.parse(new String(body));
+  Test.isTrue(bodyObj.hasOwnProperty(url));
+}
+
+function test_pipeline_unknown() {
+  var url = '/process/pipeline/notknown';
+  try {
+    var res = getResponse(url);
+    Test.fail("Should have reported an 404");
+  }
+  catch (e) {
+    Test.areEqual(404, e.javaException.getResponseCode());
+  }
+}
