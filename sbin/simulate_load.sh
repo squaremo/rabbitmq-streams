36599bdd83c8e8d379390d4bdac969ec7a700574
Michael Bridgen <mikeb@lshift.net>
2009-08-25 13:10:19 +0100

    Make this a bit more general, so we can post through it

--- sbin/simulate_load.sh
+++ sbin/simulate_load.sh
@@ -1,1 +1,5 @@
-while read line; do echo "Sending [$1:$2]: $line"; echo "$line" | nc $1 $2 ; sleep $3; done
+#!/bin/sh
+# E.g.s,
+# cat sample | simulate-load.sh "curl -d @- http://localhost:8888/livetext" 0.1
+# grep radio1 sample | simulate-load.sh "nc localhost 45678" 1
+while read line; do echo "Sending [$1]: $line"; echo "$line" | $1 ; sleep $2; done
