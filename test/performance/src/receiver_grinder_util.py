c83f6b50f8f6f58e81a6dc3a094b5fede8fe1a14
Lee Coomber <lee@lshift.net>
2009-08-25 12:02:01 +0100

    Factor out common functionality, tidy up
    --HG--
    branch : perf_test

--- test/performance/src/receiver_grinder_util.py
+++ test/performance/src/receiver_grinder_util.py
@@ -1,18 +1,7 @@
-from receiver import NetworkReceiver
-from message_store import MessageStore
-
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 
-PORT = grinder.properties.getInt("streams.test.port", 55555)
-
-MSG_STORE_HOST = grinder.properties.getProperty("streams.test.msg_store.host", "127.0.0.1")
-MSG_STORE_PORT = grinder.properties.getInt("streams.test.msg_store.port", 11211)
-
-log = grinder.logger.output
-log("Port: %i" % PORT)
-
-totalRuns = grinder.properties.getInt("grinder.runs", -1)
+from grinder_util import FeedsTestRunner
 
 # Register custom statistics
 grinder.statistics.registerDataLogExpression("Received time", "userLong0")
@@ -21,19 +10,11 @@ grinder.statistics.registerDataLogExpression("Delivery time (ms)", "userLong1")
 grinder.statistics.registerSummaryExpression("Mean delivery time (ms)",
                                              "(/ userLong1 (count timedTests))")
 
-class ReceiverTestRunner:
+class ReceiverTestRunner(FeedsTestRunner):
 
     def __init__(self, receiver):
         self._receiver = receiver
-        self._messageStore = MessageStore(MSG_STORE_HOST, MSG_STORE_PORT)
-        log("Using message store at %s:%s" % (MSG_STORE_HOST, MSG_STORE_PORT))
-
-    def getMessageStoreHost(self):
-        return MSG_STORE_HOST
-
-    def getMessageStorePort(self):
-        return MSG_STORE_PORT
-
+        FeedsTestRunner.__init__(self)
 
     def __call__(self):
         doTest = Test(1, "Receive messages").wrap(self._doTest)
@@ -69,8 +50,3 @@ class ReceiverTestRunner:
         
     def _recordDeliveryTime(self, deliveryTime):
         grinder.statistics.forCurrentTest.setLong("userLong1", deliveryTime)
-
-
-    def _getMessageId(self, message):
-        # TODO: Implement regex based ID extractor
-        return message[:1]
