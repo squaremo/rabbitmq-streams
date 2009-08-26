952465670cbd749b7bb1d4a1615b007a611cd2f5
Michael Bridgen <mikeb@lshift.net>
2009-08-26 17:52:35 +0100

    Make sure our endpoints.js endpoints match those in {sender,receiver}.js.  A Makefile target for test streams that are written in JS (but the directory still has to be rm'd manually).
    Make the chain.js test stream able to test a couple of different
    plugins, for comparison.

--- test/performance/streams/chain.js
+++ test/performance/streams/chain.js
@@ -4,16 +4,16 @@
 // 1. running one JVM per plugin
 // 2. running one JVM per feed
 
-var links = 10;
+var links = 1;
 
 var f = feed("chain");
 
 var input = f.terminal(httppost_a);
 var output = f.terminal(netprint12345);
 
-var log = f.plugin({"type": "logger", "configuration": {}});
+//var log = f.plugin({"type": "logger", "configuration": {}});
 
-f.connect(input, resource(log, "input"));
+//f.connect(input, resource(log, "input"));
 
 function js() {
   return f.plugin({"type": "javascript",
@@ -21,13 +21,31 @@ function js() {
                    {"function": "function(msg) {return msg;}"}});
 }
 
-var p = js();
+function split() {
+  return f.plugin({"type": "regexp_split",
+                   "configuration":
+                   {"regexp": ".*"}});
+}
+
+function js_conn(p, other) {
+  f.connect(resource(p, "output"), other);
+}
+
+function split_conn(p, other) {
+  f.connect(resource(p, "positive"), other);
+  f.connect(resource(p, "negative"), other);
+}
+
+var plugin = split;
+var connect = split_conn;
+
+var p = plugin();
 f.connect(input, resource(p, "input"));
 for (var i=1; i < links; i++) {
-  var p1 = js();
-  f.connect(resource(p, "output"), resource(p1, "input"));
+  var p1 = split();
+  connect(p, resource(p1, "input"));
   p = p1;
 }
-f.connect(resource(p, "output"), output);
+connect(p, output);
 
 setAllActive();
--- test/performance/streams/endpoints.js
+++ test/performance/streams/endpoints.js
@@ -1,10 +1,12 @@
 // Standard endpoints for using in test streams.
 
-var http = create_server("http", "httppost", {"http_server_port": 8888});
+var http = create_server("http", "httppost", {"http_server_port": 9876});
 var socketout = create_server("netprinter", "socket_destination", {});
 
 var httppost_a = declare_terminal("httppost_a");
-consume(http, {"url_path": "/a"}, httppost_a);
+consume(http, {"url_path": "/foo"}, httppost_a);
 
 var netprint12345 = declare_terminal("netprint12345");
 subscribe(socketout, {"port": "12345", "host": "0.0.0.0"}, netprint12345);
+
+setAllActive();
