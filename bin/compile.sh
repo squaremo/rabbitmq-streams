bc6f819af085b8e584faf3024d01f593206e9d44
Alexander Schmolck <alexander@lshift.net>
2009-08-28 16:06:44 +0100

    For completeness sake: fixed some more quoting issues in compile.sh (might matter on mac).

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-WD=`dirname $0`/compiler
-java -jar $WD/js.jar -f $WD/ast.js -f $WD/json2.js $WD/compiler.js "$@"
+WD="`dirname $0`/compiler"
+java -jar "$WD/js.jar" -f "$WD/ast.js" -f "$WD/json2.js" "$WD/compiler.js": "$@"
