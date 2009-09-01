2af8f267907db0849ae33b14794d4935a9f25bb5
Alexander Schmolck <alexander@lshift.net>
2009-09-01 16:42:04 +0100

    Added new Unavailable notification type Unavailable.
    --HG--
    branch : bug21535

--- harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
+++ harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
@@ -1,3 +1,5 @@
 package com.rabbitmq.streams.harness;
-
-public enum NotificationType { Startup, Shutdown, StopNotifier, NoData, BadData, FatalError };
+/**
+ * Unavailable refers to external resources not working/existing e.g. servers or files.
+ */
+public enum NotificationType { Startup, Shutdown, StopNotifier, NoData, BadData, FatalError, Unavailable };
