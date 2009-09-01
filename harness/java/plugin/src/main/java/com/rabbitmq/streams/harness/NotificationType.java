4026f727a69cb9e4bfab93a207d7cff575711179
Alexander Schmolck <alexander@lshift.net>
2009-09-01 16:39:14 +0100

    Got rid of more apparrently useless code, this time in NotificationType.
    --HG--
    branch : bug21535

--- harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
+++ harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
@@ -1,16 +1,3 @@
 package com.rabbitmq.streams.harness;
 
-public enum NotificationType {
-
-  Startup("Startup"), Shutdown("Shutdown"), NoData("NoData"), BadData("BadData"), FatalError("FatalError"), StopNotifier("StopNotifier");
-
-  private String label;
-
-  NotificationType(String label)  {
-    this.label = label;
-  }
-
-  public String getLabel() {
-    return label;
-  }
-}
+public enum NotificationType { Startup, Shutdown, StopNotifier, NoData, BadData, FatalError };
