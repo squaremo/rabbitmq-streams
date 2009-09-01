2e992450a543b8705aae55bfbcf25eb3ac3f16d6
Alexander Schmolck <alexander@lshift.net>
2009-09-01 15:32:37 +0100

    lastvalue.scala: got rid of apparently bogus try/catch and fixed formatting.

--- plugins/lastvalue/src/main/scala/lastvalue.scala
+++ plugins/lastvalue/src/main/scala/lastvalue.scala
@@ -19,63 +19,46 @@ class lastvalue() extends PipelineComponent() {
     // wouldn't care for it either (except as an attachment perhaps).  So we make
     // it a string.
     var cachedValue: String = null;
-    
+
     val value = new InputReader() {
-      override def handleMessage(msg: InputMessage)
-      {
+      override def handleMessage(msg: InputMessage) {
         cachedValue = new String(msg.body());
         log.debug("Got cache value " + cachedValue)
         var state : java.util.Map[String, Object] = getState();
-        
+
         if (state == null) state = new HashMap()
-        
-        if (state.contains(ValuesHeader))
-        {
-        	log.debug("CachedValue already in db")
-            val currentCache = state.get(ValuesHeader);
-            
-            if (currentCache != cachedValue)
-            {
-            	log.debug("Setting cachedvalue since it has changed from " + currentCache + " to " + cachedValue)
-              
-            	state += (ValuesHeader -> cachedValue)
-            	setState(state)
-            }
+
+        if (state.contains(ValuesHeader)) {
+          log.debug("CachedValue already in db")
+          val currentCache = state.get(ValuesHeader);
+
+          if (currentCache != cachedValue) {
+            log.debug("Setting cachedvalue since it has changed from " + currentCache + " to " + cachedValue)
+
+            state += (ValuesHeader -> cachedValue)
+            setState(state)
+          }
         }
-        else
-        {
-        	log.debug("Current value not there, setting")
-          
-        	state += (ValuesHeader -> cachedValue)
-        	setState(state)
+        else {
+          log.debug("Current value not there, setting")
+          state += (ValuesHeader -> cachedValue)
+          setState(state)
         }
-        
       }
     }
-    
     val input = new InputReader() {
-      override def handleMessage(msg: InputMessage)
-      {
-            val headers = new HashMap[String, Object]();
-            
-            if (cachedValue != null)
-            {
-                //Check message against cachedValue
-                headers += ("value" -> cachedValue);
-            }
-            
-            //Send on message possibly modified
-            try {
-              publishToChannel("output", msg.withHeader(ValuesHeader, headers));
-            } catch {
-              case e: IOException =>
-                throw new PluginException(e);
-            }
+      override def handleMessage(msg: InputMessage) {
+        val headers = new HashMap[String, Object]();
+
+        if (cachedValue != null){
+          //Check message against cachedValue
+          headers += ("value" -> cachedValue);
+        }
+        //Send on message possibly modified
+        publishToChannel("output", msg.withHeader(ValuesHeader, headers));
       }
     }
-    
     registerInput("input", input)
     registerInput("value", value)
   }
-
 }
