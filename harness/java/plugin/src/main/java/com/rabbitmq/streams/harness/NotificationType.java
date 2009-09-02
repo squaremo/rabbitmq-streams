b919a38774bb0c55bd558cd461987559d3ba0b16
Alexander Schmolck <alexander@lshift.net>
2009-09-02 14:31:16 +0100

    Wrote some preliminary NotificationType documentation.
    --HG--
    branch : bug21535

--- harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
+++ harness/java/plugin/src/main/java/com/rabbitmq/streams/harness/NotificationType.java
@@ -1,5 +1,29 @@
 package com.rabbitmq.streams.harness;
-/**
- * Unavailable refers to external resources not working/existing e.g. servers or files.
- */
-public enum NotificationType { Startup, Shutdown, StopNotifier, NoData, BadData, FatalError, Unavailable };
+
+public enum NotificationType {
+  // *** informational NotificationTypes ***
+  Startup, Shutdown, StopNotifier,
+  // *** error related NotificationTypes ***
+  /** A plugin (most likely a pipeline component) expected data but received none. */
+  NoData,
+  /** A plugin (most likely a pipeline component) received illegal data.*/
+  BadData,
+  /** Something that is in the domain of rabbit-streams itself went badly wrong
+   * and requires human intervention. 
+   * 
+   * Examples:
+   * - A misconfigured plugin.
+   * - A plugin can't access rabbit-streams resources like its database or state.
+   */
+  FatalError,
+  /** Something outside the domain of rabbit-streams itself went wrong and as a
+   * result some required resource or serive is not availabe. This will also usually
+   * require human intervention. This should normally only occur in servers.
+   *
+   * Examples:
+   * 
+   * - A XMPP gateway plugin can't connect to the XMPP server.
+   * - Some external document or URL is missing or can't be retrieved.
+   */
+  Unavailable
+};
