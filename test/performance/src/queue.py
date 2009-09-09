34f0e98f277f65945aa645c92ef92f55b01c6cc3
Michael Bridgen <mikeb@lshift.net>
2009-09-09 16:18:04 +0100

    Use the Python stdlib blocking queue rather than our own

--- test/performance/src/queue.py
+++ test/performance/src/queue.py
@@ -1,14 +1,12 @@
 import time
-
-from threading import Condition
+import Queue
 
 class TimestampedQueue:
     """Thread safe queue implementation that saves a timestamp with each queue
     item"""
     
     def __init__(self):
-        self._lock = Condition()
-        self._queue = []
+        self._queue = Queue.Queue(0)
 
     def append(self, item, timestampMs = -1):
         """Append an item to the queue"""
@@ -17,20 +15,11 @@ class TimestampedQueue:
         # assignment separately
         if timestampMs == -1:
             timestampMs = time.time() * 1000
-        
-        self._lock.acquire()
-        self._queue.insert(0, [item, timestampMs])
-        self._lock.notifyAll()
-        self._lock.release()
+        # this next won't block, since the queue size is infinite
+        self._queue.put([item, timestampMs])
 
     def pop(self):
         """Pops an item from the queue, or blocks until one is available.
         The item is returned in a list consisting of the item itself, and a
         timestamp in ms of when the item was put onto the queue."""
-        self._lock.acquire()
-        while not self._queue:
-            self._lock.wait()
-        message = self._queue.pop()
-        self._lock.release()
-        return message
-        
+        return self._queue.get()
